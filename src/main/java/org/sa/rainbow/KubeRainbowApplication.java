3de419c2151d62ca3545088b534f9e0ecf06740a
Carlos Mendes <cmendesce@gmail.com>
2019-02-01 22:39:29 -0300

    Remove znn references

--- src/main/java/org/sa/rainbow/KubeRainbowApplication.java
+++ src/main/java/org/sa/rainbow/KubeRainbowApplication.java
@@ -1,19 +1,23 @@
+package org.sa.rainbow;
+
 import org.sa.rainbow.core.RainbowDelegate;
 import org.sa.rainbow.core.RainbowMaster;
 import org.sa.rainbow.core.error.RainbowException;
-import org.sa.rainbow.gui.RainbowGUI;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.nio.file.Paths;
 
 /** @author Carlos Mendes (cmendesce@gmail.com) */
-public class ZnnApplication {
+public class KubeRainbowApplication {
+
+  private static final Logger logger = LoggerFactory.getLogger(KubeRainbowApplication.class);
 
   public static void main(String[] args) {
     try {
-
       String target = "znn";
       String config =
-          Paths.get(ZnnApplication.class.getClassLoader().getResource(target).getPath())
+          Paths.get(KubeRainbowApplication.class.getClassLoader().getResource(target).getPath())
               .getParent()
               .toString();
 
@@ -27,11 +31,8 @@ public class ZnnApplication {
       RainbowDelegate delegate = new RainbowDelegate();
       delegate.initialize();
       delegate.start();
-
-      RainbowGUI gui = new RainbowGUI(master);
-      //      gui.display();
     } catch (RainbowException e) {
-      e.printStackTrace();
+      logger.error("Cannot start rainbow.", e);
     }
   }
 }
