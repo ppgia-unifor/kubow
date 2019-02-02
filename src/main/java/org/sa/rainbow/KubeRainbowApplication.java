20ffa53304563bde9eb168904accb7972c08fe3d
Carlos Mendes <cmendesce@gmail.com>
2019-02-02 10:22:43 -0300

    Add docker build

--- src/main/java/org/sa/rainbow/KubeRainbowApplication.java
+++ src/main/java/org/sa/rainbow/KubeRainbowApplication.java
@@ -2,11 +2,14 @@ package org.sa.rainbow;
 
 import org.sa.rainbow.core.RainbowDelegate;
 import org.sa.rainbow.core.RainbowMaster;
+import org.sa.rainbow.core.error.RainbowAbortException;
 import org.sa.rainbow.core.error.RainbowException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.nio.file.Paths;
+
+import static java.text.MessageFormat.format;
 
 /** @author Carlos Mendes (cmendesce@gmail.com) */
 public class KubeRainbowApplication {
@@ -15,12 +18,28 @@ public class KubeRainbowApplication {
 
   public static void main(String[] args) {
     try {
-      String target = "znn";
-      String config =
-          Paths.get(KubeRainbowApplication.class.getClassLoader().getResource(target).getPath())
-              .getParent()
-              .toString();
 
+      var target = System.getenv("TARGET");
+      var config = System.getenv("TARGET_PATH");
+
+      if (target == null) {
+        logger.warn("No TARGET configured. Using the built in target");
+        target = "default";
+      }
+
+      if (config == null) {
+        var path = KubeRainbowApplication.class.getClassLoader().getResource(target);
+        if (path == null) {
+          var message =
+              format(
+                  "Target [{0}] does not exists in [{1}]",
+                  target, KubeRainbowApplication.class.getClassLoader().getResource(".").getPath());
+          throw new RainbowAbortException(message);
+        }
+        config = Paths.get(path.getPath()).getParent().toString();
+      }
+
+      logger.info("Using target [{}] located in path [{}]", target, config);
       System.setProperty("rainbow.config", config);
       System.setProperty("rainbow.target", target);
 
