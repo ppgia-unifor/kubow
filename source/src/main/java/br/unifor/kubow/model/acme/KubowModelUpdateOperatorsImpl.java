81f774558ace23fd31cfd9b230d3752f07523233
Carlos Mendes <cmendesce@gmail.com>
2020-07-07 11:03:23 -0300

    Rename from old naming strategy

--- source/src/main/java/br/unifor/kubow/model/acme/KubowModelUpdateOperatorsImpl.java
+++ source/src/main/java/br/unifor/kubow/model/acme/KubowModelUpdateOperatorsImpl.java
@@ -4,11 +4,11 @@ import org.acmestudio.acme.element.IAcmeSystem;
 import org.sa.rainbow.model.acme.AcmeModelInstance;
 import br.unifor.kubow.model.acme.commands.KubowCommandFactory;
 
-public class KubeModelUpdateOperatorsImpl extends AcmeModelInstance {
+public class KubowModelUpdateOperatorsImpl extends AcmeModelInstance {
 
   private KubowCommandFactory m_commandFactory;
 
-  public KubeModelUpdateOperatorsImpl(IAcmeSystem system, String source) {
+  public KubowModelUpdateOperatorsImpl(IAcmeSystem system, String source) {
     super(system, source);
     // Make sure it is the right family
   }
@@ -23,6 +23,6 @@ public class KubeModelUpdateOperatorsImpl extends AcmeModelInstance {
 
   @Override
   protected AcmeModelInstance generateInstance(IAcmeSystem sys) {
-    return new KubeModelUpdateOperatorsImpl(sys, getOriginalSource());
+    return new KubowModelUpdateOperatorsImpl(sys, getOriginalSource());
   }
 }
